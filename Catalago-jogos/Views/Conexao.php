f262eaca24015d761fa38e8a008be2995f95604a
Diego <diegoubatuba103@gmail.com>
2025-04-21 20:52:38 -0300

    full code

---
+++ Catalago-jogos/Views/Conexao.php
@@ -0,0 +1,195 @@
+<?php
+class Conexao {
+    private $instancia;
+    
+    public function __construct()
+    {
+        
+
+    }
+    public function getConexao()
+    {
+        $dbname = 'dbjogos';
+        $host = '127.0.0.1';
+        $user = "root";
+        $pas = '';
+        try{
+            $this->instancia = new PDO('mysql:dbname='.$dbname.';host='.$host,$user,$pas);
+        }catch(PDOException $e){
+            echo "erro".$e->getMessage();
+        }
+        
+    }
+    public function getJogos(){
+        $dados = array();
+        $stmt = $this->instancia->query('SELECT * FROM playstation join requisitos_recomendados ON playstation.fk_id = requisitos_recomendados.id');
+
+        $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // $jogojosn=json_encode($dados);
+        // $arq = fopen("dadosjg.json",'w+');
+        // fwrite($arq,$jogojosn);
+        // fclose($arq);
+        return $dados;
+       
+    }
+    public function getJogoId($nome_jogo){
+        $jogo=array();
+        $cmd = $this->instancia->prepare('SELECT * FROM playstation WHERE nome_jogo = :nomejogo');
+         $nomejg = implode(":",$nome_jogo);
+         $cmd->bindValue(':nomejogo',$nomejg);
+         $cmd->execute();
+        $jogo = $cmd->fetch(PDO::FETCH_ASSOC);
+        // $jogojosn=json_encode($jogo);
+        // $arq = fopen("dadosjg.json",'w+');
+        // fwrite($arq,$jogojosn);
+        // fclose($arq);
+        // $json = json_encode($jogo);
+        return $jogo;
+    }
+
+    public function cadastrar($email,$senha){
+        //verificar se ja esta cadastrado
+        $sql = $this->instancia->prepare("SELECT id FROM usuarios WHERE email = :email");
+        $sql->bindValue(":email",$email);
+        $sql->execute();
+        if($sql->rowCount()>0)
+        {
+            return false;
+        }
+        else
+        {
+            //caso não, cadastrar
+            $sql = $this->instancia->prepare("INSERT INTO usuarios (email,senha) VALUES (:email,:senha)");
+            $sql->bindValue(":email",$email);
+            $sql->bindValue(":senha",$senha);
+            $sql->execute();
+            return true;
+        }   
+        
+
+    }
+
+    public function logar($email,$senha){
+        $sql = $this->instancia->prepare('SELECT id FROM usuarios WHERE email = :email AND senha = :senha ');
+        $sql->bindValue(":email",$email);
+        $sql->bindValue(":senha",$senha);
+        $sql->execute();
+        if($sql->rowCount()>0)
+        {
+            $dados=$sql->fetch();
+            session_start();
+            $_SESSION['id_usuario']=$dados['id'];
+            return true;
+        }
+        else{
+            return false;
+        }
+
+    }
+    public function getJogosplay(){
+        $dados = array();
+        $stmt = $this->instancia->query('SELECT * FROM playstation');
+
+        $dados = $stmt->fetchAll(PDO::FETCH_ASSOC);
+        // $jogojosn=json_encode($dados);
+        // $arq = fopen("dadosjg.json",'w+');
+        // fwrite($arq,$jogojosn);
+        // fclose($arq);
+        return $dados;
+       
+    }
+
+    public function delete($id){
+        $stmt = $this->instancia->prepare('SELECT nome_imagem FROM playstation WHERE id = :id');
+        $stmt->bindValue(':id',$id);
+        $stmt->execute();
+        $nomeImg = $stmt->fetch(PDO::FETCH_ASSOC);
+        if(is_file('../Midia/img/'.$nomeImg['nome_imagem'])){
+            unlink('../Midia/img/'.$nomeImg['nome_imagem']);
+            $stmt = $this->instancia->prepare('DELETE FROM playstation WHERE id = :id');
+            $stmt->bindValue(':id',$id);
+            $stmt->execute();
+            
+        }else{
+            if(!is_file('../Midia/img/'.$nomeImg['nome_imagem'])){
+                echo'a imagem não existe';
+                $stmt = $this->instancia->prepare('DELETE FROM playstation WHERE id = :id');
+                $stmt->bindValue(':id',$id);
+                $stmt->execute();
+            }
+            echo'deu errado';
+            
+        }
+        //  $stmt = $this->instancia->prepare('DELETE FROM playstation WHERE id = :id');
+
+        //  $stmt->bindValue(':id',$id);
+        //  $stmt->execute();
+    }
+     
+
+
+    public function editarJogoPlay($id){
+        $stmt = $this->instancia->prepare('SELECT p.nome,p.descricao,p.nome_imagem,p.genero,p.id,p.fk_id,r.OS,r.processador,r.placa_video,r.memoria,r.armazenamento FROM playstation as p join requisitos_recomendados as r ON p.fk_id = r.id where fk_id = :id');
+        $stmt->bindValue(':id',$id);
+        $stmt->execute();
+        if($stmt->rowCount() > 0){
+            $dados = $stmt->fetch(PDO::FETCH_ASSOC);
+            return $dados;
+        }else{
+            header('location: admin.php');
+        }
+    }
+    public function updateJogosPlay($id,$nome,$desc,$img){
+        $stmt = $this->instancia->prepare('UPDATE playstation SET nome = :nome, descricao = :descricao, nome_imagem = :img WHERE id = :id');
+        $stmt->bindValue(':nome',$nome);
+        $stmt->bindValue('descricao',$desc);
+        $stmt->bindValue(':img',$img);
+        $stmt->bindValue(':id',$id);
+        $stmt->execute();
+        //header('location:admin.php');
+        
+    }
+
+    public function cadastrarJogo($nome, $descr, $tabela, $img){
+
+        if($tabela == 'playstation'){
+            $stmt = $this->instancia->prepare("INSERT INTO playstation (nome, descricao,nome_imagem ) values (:nome, :descr, :img)");
+            $stmt->bindValue(':nome',$nome);
+            $stmt->bindValue(':descr',$descr);
+            $stmt->bindValue(':img',$img);
+            // $stmt->bindValue(':img',$img);
+            $stmt->execute();
+            header('location:admin.php');
+            exit;
+        }elseif($tabela == 'nitendo'){
+            $stmt = $this->instancia->prepare("INSERT INTO nitendo (nome, descricao) values (:nome, :descr)");
+            $stmt->bindValue(':nome',$nome);
+            $stmt->bindValue(':descr',$descr);
+            // $stmt->bindValue(':img',$img);
+            $stmt->execute();
+            header('location:admin.php');
+            exit;
+        }elseif($tabela == 'pc'){
+            $stmt = $this->instancia->prepare("INSERT INTO pc (nome, descricao) values (:nome, :descr)");
+            $stmt->bindValue(':nome',$nome);
+            $stmt->bindValue(':descr',$descr);
+            // $stmt->bindValue(':img',$img);
+            $stmt->execute();
+            header('location:admin.php');
+            exit;
+        }elseif($tabela == 'xbox'){
+            $stmt = $this->instancia->prepare("INSERT INTO xbox (nome, descricao) values (:nome, :descr)");
+            $stmt->bindValue(':nome',$nome);
+            $stmt->bindValue(':descr',$descr);
+            // $stmt->bindValue(':img',$img);
+            $stmt->execute();
+            header('location:admin.php');
+            exit;
+        }
+       
+    }
+}
+    
+    
+
+?>
---
+++ Catalago-jogos/Views/cards.js
@@ -0,0 +1,262 @@
+function carregar(){
+    fetch('../dados.json').then(response=>response.json()).then(jogos=>{
+        const conteiner = document.querySelector(".conteiner")
+
+        jogos.map(jogo=>{
+            const card = document.createElement("div")
+            card.classList.add("card")
+
+            const caminhoImg = '../Midia/img/' + jogo.nome_imagem
+            const img = document.createElement("img")
+            img.classList.add("img")
+            img.src = caminhoImg
+            img.alt = jogo.nome_imagem
+
+            
+
+            const btn = document.createElement('button')
+            btn.classList.add(jogo.id)
+
+            const conteiner_img = document.createElement("div")
+            conteiner_img.classList.add("conteiner_img")
+
+            btn.textContent = jogo.nome
+
+
+            card.appendChild(conteiner_img)
+            conteiner_img.appendChild(img)
+            card.appendChild(btn)
+            conteiner.appendChild(card)
+            
+        })
+    })
+}
+carregar();
+
+document.addEventListener("click",(e)=>{
+    
+   
+    const conts = e.target.className
+    
+ 
+     fetch('../dados.json')
+     .then(response=>response.json())
+     .then(jogos=>{
+ 
+         
+ 
+         const conteiner = document.querySelector(".conteiner")
+        
+         
+         jogos.map(jogo=>{
+             if(conts == jogo.id){
+ 
+                 const card = document.createElement("div")
+                 card.classList.add("con-jogo")
+ 
+                 conteiner.appendChild(card)
+
+                 const img = document.createElement("img");
+                 img.classList.add("img-jg")
+                 img.src = '../Midia/img/' + jogo.nome_imagem
+                 
+                 const conteiner_img = document.createElement("div");
+                 conteiner_img.classList.add('imagem')
+                 conteiner_img.appendChild(img)
+                 card.appendChild(conteiner_img)
+
+                 
+ 
+                 const title = document.createElement("h1")
+                 title.textContent = jogo.nome
+                 title.classList.add("title-jogo")
+                //  card.appendChild(title)
+                 const conteiner_desc = document.createElement("div");
+                 conteiner_desc.classList.add("conteiner-desc")
+                 conteiner_desc.appendChild(title)
+
+                 //genero
+                 const divGeneroJogo = document.createElement('div')
+                 divGeneroJogo.classList.add('divGeneroJogo')
+                
+
+                 const generoJogoTitle = document.createElement('h3')
+                 generoJogoTitle.classList.add('generoJogoTitle')
+                 generoJogoTitle.textContent= 'Gêneros:'
+                 divGeneroJogo.appendChild(generoJogoTitle)
+
+                 const generoJogo = document.createElement('p')
+                 generoJogo.classList.add('generoJogo')
+                 generoJogo.textContent= jogo.genero
+                 divGeneroJogo.appendChild(generoJogo)
+                 conteiner_desc.appendChild(divGeneroJogo)
+
+
+                 //generofim
+
+                 //descrição
+                 const divDescricao = document.createElement('div')
+                 divDescricao.classList.add('divDescricao')
+
+                 const titleDescricao = document.createElement('h3')
+                 titleDescricao.classList.add('titleDescricao')
+                 titleDescricao.textContent = 'Descrição:'
+
+                 const desc = document.createElement("p");
+                 desc.classList.add("desc-jg")
+                 desc.textContent = jogo.descricao
+                 divDescricao.appendChild(titleDescricao)
+                 divDescricao.appendChild(desc)
+                 conteiner_desc.appendChild(divDescricao)
+                 //fim descrição
+
+                
+
+                 const conteiner_requistos = document.createElement("div");
+                 conteiner_requistos.classList.add("requisitos")
+                 conteiner_desc.appendChild(conteiner_requistos)
+
+                 const titulo_requisitos =document.createElement('h3')
+                 titulo_requisitos.textContent = 'Requisitos Recomendados'
+                 conteiner_requistos.appendChild(titulo_requisitos)
+
+
+                 const processador = document.createElement('p')
+                 const placa_video = document.createElement('p')
+                 const memoria = document.createElement('p')
+                 const armazenamento = document.createElement('p')
+                 const os = document.createElement('p')
+
+                 os.textContent = jogo.OS
+                 placa_video.textContent = jogo.placa_video
+                 processador.textContent = jogo.processador
+                 memoria.textContent = jogo.memoria
+                 armazenamento.textContent = jogo.armazenamento
+                 
+                 const title_Processador = document.createElement('strong')
+                 title_Processador.textContent = 'Processador: '
+
+                 const title_PlacaDeVideo = document.createElement('strong')
+                 title_PlacaDeVideo.textContent = 'Placa De Video: '
+
+                 const title_memoria = document.createElement('strong')
+                 title_memoria.textContent = 'Memoria: '
+
+                 const title_armazenamento = document.createElement('strong')
+                 title_armazenamento.textContent = 'Armazenamento: '
+
+                 const title_os = document.createElement('strong')
+                 title_os.textContent = 'Os: '
+                 
+                
+                
+                //title do rquisito
+                //processador
+                const divProcessador = document.createElement('div')
+                divProcessador.classList.add('divRequisitos')
+                const topicoProcessador  = document.createElement('div');
+                topicoProcessador.classList.add('topico')
+                topicoProcessador.appendChild(title_Processador)
+                divProcessador.appendChild(topicoProcessador)
+                const divdados = document.createElement('div')
+                divdados.classList.add('divdados')
+                divdados.appendChild(processador)
+                divProcessador.appendChild(divdados)
+                conteiner_requistos.appendChild(divProcessador)
+                //processador fim
+                //placa de video
+                const topicoPlacaDeVideo = document.createElement('div');
+                topicoPlacaDeVideo.classList.add('topico')
+                topicoPlacaDeVideo.appendChild(title_PlacaDeVideo)
+                const divPlacaDeVideo = document.createElement('div')
+                divPlacaDeVideo.classList.add('divRequisitos')
+                divPlacaDeVideo.appendChild(topicoPlacaDeVideo )
+                divPlacaDeVideo.appendChild(placa_video)
+                conteiner_requistos.appendChild(divPlacaDeVideo)
+                //placa de video fim
+                //armazenamento
+                const topicoArmazenamento = document.createElement('div');
+                topicoArmazenamento.classList.add('topico')
+                topicoArmazenamento.appendChild(title_armazenamento)
+                const divArmazenamento = document.createElement('div')
+                divArmazenamento.classList.add('divRequisitos')
+                conteiner_requistos.appendChild(divArmazenamento)
+                divArmazenamento.appendChild(topicoArmazenamento )
+                divArmazenamento.appendChild(armazenamento)
+                conteiner_requistos.appendChild(divArmazenamento)
+                //fimArmazenamento
+                //memoria
+                const topicoMemoria = document.createElement('div');
+                topicoMemoria.classList.add('topico')
+                topicoMemoria.appendChild(title_memoria)
+                const divMemoria = document.createElement('div')
+                divMemoria.classList.add('divRequisitos')
+                conteiner_requistos.appendChild(divMemoria)
+                divMemoria.appendChild(topicoMemoria )
+                divMemoria.appendChild(memoria)
+                conteiner_requistos.appendChild(divMemoria)
+                //memoria fim
+                //sismeta operacional
+                const topicoOs = document.createElement('div');
+                topicoOs.classList.add('topico')
+                topicoOs.appendChild(title_os)
+                const divOs = document.createElement('div')
+                divOs.classList.add('divRequisitos')
+                conteiner_requistos.appendChild(divOs)
+                divOs.appendChild(topicoOs )
+                divOs.appendChild(os)
+                conteiner_requistos.appendChild(divOs)
+                //sistema operacional
+
+                //fim title requisito
+                
+
+                 
+
+                 
+                 
+                 
+
+                
+                //  divrequisitos.appendChild(processador)
+                //  divrequisitos.appendChild(memoria)
+                //  divrequisitos.appendChild(armazenamento)
+                //  divrequisitos.appendChild(os)
+                 
+
+
+                 card.appendChild(conteiner_desc)
+ 
+                 const fechar = document.createElement("div")
+                 fechar.classList.add("fechar")
+                 conteiner.appendChild(fechar)
+                 
+                 const X = document.createElement("h3")
+                 X.classList.add("fx")
+                 X.textContent = 'X'
+ 
+                 fechar.appendChild(X)
+                 card.appendChild(fechar)
+                 
+                 document.addEventListener("click",(e)=>{
+                 const fexar = e.target.className
+                 if(fexar == 'fx'){
+                     conteiner.removeChild(card)
+                     
+                 }
+ 
+                 })
+ 
+ 
+ 
+ 
+ 
+ 
+             }
+ 
+         })
+ 
+     
+     })
+ 
+ })
